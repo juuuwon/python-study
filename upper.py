97073572261ce65f22c30ec48d49983bb123e61c
juuuwon <juuuwon@naver.com>
2019-04-04 18:50:46 +0900

    new file created

---
+++ upper.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+myList = ["apple","banana","mango","melon"]
+for each in myList:
+            print(each.upper())
+    
+
+   
+
+
+
+    
+
+    
+   
+    
+
+    
+  
